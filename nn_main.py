9a3015aed82835cb7426c593c03d699d8df48916
Songbin <songbinliu@hotmail.com>
2018-05-11 16:46:16 -0400

    Update nn_main.py
    change the result of range to list for python3

--- nn_main.py
+++ nn_main.py
@@ -50,7 +50,7 @@ def train_it(nn, train_data, lr):
     imgs = train_data[1]
 
     # shuffle the data
-    alist = range(labels.shape[0])
+    alist = list(range(labels.shape[0]))
     shuffle(alist)
 
     for i in alist:
